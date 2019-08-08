a23f9a3681ab4f4549c3f7372a2425fcdb4ad794
walter <walter_rfl@hotmail.com>
2019-08-08 17:06:23 -0300

    continuação tela formulários..

--- app/src/main/java/br/com/wab/equest_app_mvvm/view/SurveyActivity.kt
+++ app/src/main/java/br/com/wab/equest_app_mvvm/view/SurveyActivity.kt
@@ -1,13 +1,25 @@
 package br.com.wab.equest_app_mvvm.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import br.com.wab.equest_app_mvvm.BaseActivity
 import br.com.wab.equest_app_mvvm.R
+import br.com.wab.equest_app_mvvm.databinding.ActivitySurveyBinding
+import br.com.wab.equest_app_mvvm.viewmodel.SurveyViewModel
 
-class SurveyActivity : AppCompatActivity() {
+class SurveyActivity : BaseActivity() {
+
+    lateinit var viewModel: SurveyViewModel
+    lateinit var binding: ActivitySurveyBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_survey)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_survey)
+        viewModel = ViewModelProviders.of(this@SurveyActivity).get(SurveyViewModel::class.java)
+        binding.viewmodel = viewModel
+
+
     }
 }
--- app/src/main/java/br/com/wab/equest_app_mvvm/viewmodel/SurveyViewModel.kt
+++ app/src/main/java/br/com/wab/equest_app_mvvm/viewmodel/SurveyViewModel.kt
@@ -1,10 +1,10 @@
 package br.com.wab.equest_app_mvvm.viewmodel
 
-import androidx.databinding.BaseObservable
 import androidx.databinding.ObservableField
+import androidx.lifecycle.ViewModel
 import br.com.wab.equest_app_mvvm.model.Survey
 
-class SurveyViewModel: BaseObservable() {
+class SurveyViewModel: ViewModel() {
 
     val survey = ObservableField<Survey>()
 
